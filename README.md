3d905aea62c6f0c592f1fb314048e49e68bde3b2
Yerin Cha <lenacha32@gmail.com>
2019-05-29 20:00:47 -0700

    Proxy server for Hacker-Home

---
+++ README.md
@@ -0,0 +1,34 @@
+# Project Name
+
+> Project description
+
+## Related Projects
+
+  - https://github.com/hacker-home/Airbnb-more-homes
+  - https://github.com/hacker-home/Airbnb-info
+  - https://github.com/hacker-home/Airbnb-reviews
+  - https://github.com/hacker-home/Airbnb-photos
+
+## Usage
+
+> Proxy server for Hacker-home Airbnb FEC project
+
+## Requirements
+- Node 6.13.0
+- Google Chrome browser
+
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+$ npm install
+```
+
+- git clone all related project
+- run all the local server of the related projects
+- build bundle.js with webpack for each modules
+- npm start (run proxy server)
+- open the browser with "localhost:3000"
+
---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// Config
+const { routes } = require('./config.json');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+app.use(cors());
+
+for (route of routes) {
+  app.use(route.route,
+    proxy({
+      target: route.address,
+      pathRewrite: (path, req) => {
+        return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+      }
+    })
+  );
+}
+
+app.listen(3000, () => {
+  console.log('Proxy listening on port 3000');
+});
